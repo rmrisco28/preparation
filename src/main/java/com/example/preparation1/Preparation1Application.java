a7d88d579245b0468809dde1c3dd776bee8cde7d
rmrisco28 <rmrisco28@naver.com>
2025-06-20 16:54:58 +0900

    first commit

---
+++ src/main/java/com/example/preparation1/Preparation1Application.java
@@ -0,0 +1,13 @@
+package com.example.preparation1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Preparation1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Preparation1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/preparation1/Preparation1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preparation1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Preparation1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
